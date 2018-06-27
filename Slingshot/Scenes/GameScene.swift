78db0b9d3f522d549ecbc63d8b3a407acb555efc
Anish Ali <ali.anish91@yahoo.com>
2018-06-27 19:23:55 -0400

    Added a pan gesture to the view

--- Slingshot/Scenes/GameScene.swift
+++ Slingshot/Scenes/GameScene.swift
@@ -12,15 +12,31 @@ import GameplayKit
 class GameScene: SKScene {
     
     let gameCamera = SKCameraNode()
+    var panRecognizer = UIPanGestureRecognizer()
     
     override func didMove(to view: SKView) {
         addCamera()
+        setupGestureRecognizers()
     }
     
+    func setupGestureRecognizers() {
+        guard let view = view else { return }
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        view.addGestureRecognizer(panRecognizer)
+    }
     func addCamera() {
         guard let view = view else { return }
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
         camera = gameCamera
+    }
+}
+
+extension GameScene {
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view)
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
     }
 }
