1936db2a86f6fed51a49a989caba7dbd397c1cd6
Anish Ali <ali.anish91@yahoo.com>
2018-06-28 19:42:32 -0400

    Adding a rock class with implementation

---
+++ Slingshot/Custom Nodes/Rock.swift
@@ -0,0 +1,40 @@
+//
+//  Rock.swift
+//  Slingshot
+//
+//  Created by user139990 on 6/28/18.
+//  Copyright © 2018 Anish Ali. All rights reserved.
+//
+
+import SpriteKit
+
+
+enum RockType: String {
+    case red, blue, yellow, gray
+}
+class Rock: SKSpriteNode {
+    
+    let rockType: RockType
+    
+    init(type: RockType) {
+        rockType = type
+        
+        let color: UIColor!
+        switch type {
+        case .red:
+            color = UIColor.red
+        case .blue:
+            color = UIColor.blue
+        case .yellow:
+            color = UIColor.yellow
+        case .gray:
+            color = UIColor.lightGray
+        }
+        super.init(texture: nil, color: color, size: CGSize(width: 40.0, height: 40.0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
