903b2a8f3d9a9f16aa6476c2431f1cd185f234d2
Anish Ali <ali.anish91@yahoo.com>
2018-07-09 17:09:05 -0400

    added files for menu and level scene

---
+++ Slingshot/Scenes/LevelScene.swift
@@ -0,0 +1,13 @@
+//
+//  LevelScene.swift
+//  Slingshot
+//
+//  Created by user139990 on 7/9/18.
+//  Copyright © 2018 Anish Ali. All rights reserved.
+//
+
+import SpriteKit
+
+class LevelScene: SKScene {
+
+}
---
+++ Slingshot/Scenes/MenuScene.swift
@@ -0,0 +1,13 @@
+//
+//  MenuScene.swift
+//  Slingshot
+//
+//  Created by user139990 on 7/9/18.
+//  Copyright © 2018 Anish Ali. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+
+}
