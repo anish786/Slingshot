236144adb2fb22e4b3e747fdf19de6f52adb93b1
Anish Ali <ali.anish91@yahoo.com>
2018-07-09 17:33:24 -0400

    added play button to the menu scene which on click take to the level scene

--- Slingshot/Scenes/MenuScene.swift
+++ Slingshot/Scenes/MenuScene.swift
@@ -11,5 +11,19 @@ import SpriteKit
 class MenuScene: SKScene {
     
     var sceneManagerDelegate: SceneManagerDelegate?
+    
+    override func didMove(to view: SKView) {
+        setupMenu()
+    }
 
+    func setupMenu() {
+        let button = SpriteKitButton(defaultButtonImage: "playButton", action: goToLevelScene, index: 0)
+        button.position = CGPoint(x: frame.midX, y: frame.midY)
+        button.aspectScale(to: frame.size, width: false, multiplier: 0.2)
+        addChild(button)
+    }
+    
+    func goToLevelScene(_: Int) {
+        sceneManagerDelegate?.presentLevelScene()
+    }
 }
